0c6cbe591d4a839aa4cea76294c3f432da3da88f
mrsdno <christine@dno.dev>
2022-06-07 17:05:16 -0400

    added comment CRUD routes

--- models/User.js
+++ models/User.js
@@ -4,7 +4,6 @@ const bcrypt = require('bcrypt');
 
 
 // create the user model
-// create our User model
 class User extends Model {
     // set up method to run on instance data (per user) to check password
     checkPassword(loginPw) {
