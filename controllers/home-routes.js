124ddb83dc4cf70b7fc20ff6dc68d5902e4c4561
mrsdno <christine@dno.dev>
2022-06-07 22:15:58 -0400

    add posts to homepage, styling of homepage and login page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,19 +1,44 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const { Post, User, Comment } = require("../models");
 
-router.get('/', (req, res) => {
-    res.render('homepage', {
-        // pass data to handlebars page
-        loggedIn: req.session.loggedIn,
+router.get("/", (req, res) => {
+  Post.findAll({
+    attributes: ["id", "post_text", "title", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  }).then((dbPostData) => {
+    const posts = dbPostData.map((post) => post.get({ plain: true }));
+    res.render("homepage", {
+      // pass data to handlebars page
+        posts,
+      loggedIn: req.session.loggedIn,
     });
+  })
+      .catch((err) => {
+          console.log(err);
+          res.status(500).json(err);
+    })
 });
 
-router.get('/login', (req, res) => {
-    if (req.session.loggedIn) {
-        res.redirect('/');
-        return;
-    }
-    
-    res.render('login');
-})
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("login");
+});
 
 module.exports = router;
